dd3702f2693d39b38efb7b2363db282fcda90629
sprintqaDevOps <77872304+sprintqaDevOps@users.noreply.github.com>
2021-02-13 01:16:05 -0700

    Update ec2DailyStart.py

--- awsLambda/ec2DailyStart.py
+++ awsLambda/ec2DailyStart.py
@@ -1,1 +1,23 @@
-print("starting ec2s daily at 8am")
+def startEC2():    
+    print("starting ec2s daily at 8am")
+    # Get list of regions
+    regions = [region['RegionName']
+    for region in ec2_client.describe_regions()['Regions']]
+
+    # Iterate over each region
+    for region in regions:
+        ec2 = boto3.resource('ec2', region_name=region)
+
+        print("Region:", region)
+
+        # Get only running instances
+        instances = ec2.instances.filter(
+            Filters=[{'Name': 'instance-state-name',
+                      'Values': ['stopped']}])
+
+        # Stop the instances
+        for instance in instances:
+            instance.start()
+            print('Started instance: ', instance.id)
+  
+startEC2()
